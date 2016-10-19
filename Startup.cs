b9760b51052b5248aba5a3d38d25f8ac118f458b
Zhong Dian <ceo@myaiol.com>
2016-10-19 21:44:40 +0800

    Introduce Pomelo's MySQL connector

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetcoreapp
 {
@@ -12,6 +13,7 @@ namespace aspnetcoreapp
         public void Configure(IApplicationBuilder app)
         {
             app.UseMvc();
+            app.UseDeveloperExceptionPage();
         }
 
         public void ConfigureServices(IServiceCollection services)
